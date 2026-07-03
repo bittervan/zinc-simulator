87301fdef2144ba536afeb8d9e574cfc4d3f35f9
Xingkai Wang <xingkai.wang@outlook.com>
2026-07-03 10:57:20 +0800

    Archive before changing to elfio

--- include/elf_loader.h
+++ include/elf_loader.h
@@ -1,10 +1,15 @@
 #pragma once
 #include <string>
 #include <filesystem>
+#include <vector>
+#include <cstdint>
 
 class ElfLoader {
 
 public:
     ElfLoader(const std::filesystem::path &elf_path);
+
+private:
+    std::vector<uint8_t> file_bytes;
 
 };
--- src/elf_loader.cpp
+++ src/elf_loader.cpp
@@ -1,8 +1,12 @@
 #include <elf_loader.h>
 #include <fstream>
+#include <ios>
 #include <iostream>
 
 ElfLoader::ElfLoader(const std::filesystem::path &elf_path) {
     std::ifstream elf_file(elf_path, std::ios::binary | std::ios::ate);
-    std::cout << elf_file.tellg() << std::endl;
+    const std::streamsize size = elf_file.tellg();
+    file_bytes.resize(size);
+    elf_file.seekg(0, std::ios::beg);
+    elf_file.read(reinterpret_cast<char*>(file_bytes.data()), size);
 }
